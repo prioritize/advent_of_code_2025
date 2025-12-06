0d5d5a639a1535c1c2b611c54261a1a8fdb86e83
prioritize <jared@prototypic.al>
2025-12-05 16:24:08 -0800

    working on part 2

--- src/day_1/padlock.rs
+++ src/day_1/padlock.rs
@@ -3,7 +3,7 @@ use std::{
     collections::VecDeque,
     fs::File,
     io::{BufRead, BufReader},
-    usize,
+    u32, usize,
 };
 #[derive(Debug)]
 pub struct Rotation {
@@ -30,6 +30,40 @@ pub enum Direction {
 pub struct Padlock {
     dial: VecDeque<u32>,
     current: usize,
+}
+fn parse_line_to_rotation_tracked(l: String) -> (Rotation, usize) {
+    let length = l.len();
+    let dir = match l.chars().nth(0) {
+        Some(f) => match f {
+            'R' => Direction::R,
+            'L' => Direction::L,
+            _ => panic!("Got an incorrect direction"),
+        },
+        None => panic!("Found an empty line"),
+    };
+    let offset: (usize, usize) = match length {
+        0..2 => {
+            panic!("Got a bad line")
+        }
+
+        2 => match l.chars().nth(1) {
+            Some(x) => (x.to_digit(10).unwrap() as usize, 0),
+            None => panic!("Got bad input"),
+        },
+        3 => {
+            let tens = l.chars().nth(length - 2).unwrap().to_digit(10).unwrap() as usize;
+            let ones = l.chars().nth(length - 1).unwrap().to_digit(10).unwrap() as usize;
+            (tens * 10 + ones, 0)
+        }
+        4.. => {
+            let number: usize = l[1..].parse().unwrap();
+            let rotations = number / 100;
+            let upper = get_tens_value(number);
+            let lower = get_ones_value(number);
+            (upper * 10 + lower, rotations)
+        }
+    };
+    (Rotation::new(dir, offset.0), offset.1)
 }
 fn parse_line_to_rotation(l: String) -> Rotation {
     let length = l.len();
@@ -76,6 +110,24 @@ pub fn parse_day_1_input(f_name: &str) -> Result<Vec<Rotation>> {
     lines.iter().for_each(|x| println!("{x:?}"));
     Ok(lines)
 }
+pub fn parse_day_1_input_tracked(f_name: &str) -> Result<Vec<(Rotation, usize)>> {
+    let file = File::open(f_name)?;
+    let reader = BufReader::new(file);
+    let lines = reader
+        .lines()
+        .map(|l| {
+            if let Ok(line) = l {
+                parse_line_to_rotation_tracked(line)
+            } else {
+                println!("Got bad input");
+                panic!("Bad input");
+            }
+        })
+        .collect::<Vec<(Rotation, usize)>>();
+
+    lines.iter().for_each(|x| println!("{x:?}"));
+    Ok(lines)
+}
 
 impl Padlock {
     pub fn new() -> Self {
@@ -89,7 +141,7 @@ impl Padlock {
             current: 50,
         }
     }
-    pub fn get_next_location(&self, rot: Rotation) -> usize {
+    pub fn get_next_location(&self, rot: &Rotation) -> usize {
         match rot.dir {
             Direction::L => {
                 // TODO: Handle the left case
@@ -131,9 +183,73 @@ impl Padlock {
             }
         }
     }
-    pub fn rotate(&mut self, rot: Rotation) {
+    pub fn get_next_location_tracked(&self, rot: &Rotation) -> (usize, bool) {
+        match rot.dir {
+            Direction::L => match self.current.checked_sub(rot.offset) {
+                Some(loc) => {
+                    println!("left rotation after: {loc}");
+                    (loc, false)
+                }
+                None => match rot.offset.checked_sub(self.current) {
+                    Some(of) => (self.dial.len() - of, true),
+                    None => {
+                        println!("We shouldn't be getting here! We've failed on both directions");
+                        panic!("Failed in rotating left");
+                    }
+                },
+            },
+            Direction::R => match upper_clamp(self.current + rot.offset, 99) {
+                Some(loc) => (loc, false),
+                None => {
+                    let to_zero = 99 - self.current;
+                    let remaining = rot.offset - to_zero;
+                    println!(
+                        "values: offset: {}, to_zero: {to_zero}, remaining: {remaining}",
+                        rot.offset
+                    );
+                    (remaining - 1, true)
+                }
+            },
+        }
+    }
+    pub fn rotate(&mut self, rot: &Rotation) {
         let next = self.get_next_location(rot);
         self.current = next;
+    }
+    pub fn rotate_tracked(&mut self, rot: &Rotation) -> bool {
+        let start = self.current;
+        let (next, hit_zero) = self.get_next_location_tracked(rot);
+        self.current = next;
+        if hit_zero {
+            println!(
+                "Starting at {} we hit zero when performing an {:?} for {} rotations",
+                start, rot.dir, rot.offset
+            );
+        }
+        hit_zero
+    }
+    pub fn evaluate(&mut self, ops: &[Rotation]) -> u32 {
+        let mut acc = 0;
+        for op in ops {
+            self.rotate(op);
+            if self.current == 0 {
+                acc += 1;
+            }
+        }
+        acc
+    }
+    pub fn evaluate_tracked(&mut self, ops: &[(Rotation, usize)]) -> u32 {
+        let mut acc = 0;
+        for (rot, rotations) in ops {
+            if self.rotate_tracked(rot) {
+                acc += 1
+            }
+            if rotations > &0 {
+                println!("Rotations: {rotations}");
+            }
+            acc += *rotations as u32;
+        }
+        acc
     }
 }
 // This function takes an input and evaluates it against the upper bound.
@@ -169,26 +285,21 @@ mod tests {
     #[test]
     fn test_get_next_location() {
         let padlock = Padlock::new();
-        assert_eq!(
-            padlock.get_next_location(Rotation {
-                dir: Direction::R,
-                offset: 50
-            }),
-            0
-        );
+        let rot = Rotation::new(Direction::R, 50);
+        assert_eq!(padlock.get_next_location(&rot), 0);
     }
     #[test]
     fn test_rollover() {
         let mut padlock = Padlock::new();
         padlock.current = 99;
         let rot = Rotation::new(Direction::R, 1);
-        assert_eq!(padlock.get_next_location(rot), 0);
+        assert_eq!(padlock.get_next_location(&rot), 0);
     }
     #[test]
     fn test_n_rollover() {
         let padlock = Padlock::new();
         let rot = Rotation::new(Direction::R, 200);
-        let endpoint = padlock.get_next_location(rot);
+        let endpoint = padlock.get_next_location(&rot);
         assert_eq!(endpoint, 50);
     }
     #[test]
@@ -220,14 +331,14 @@ mod tests {
         let mut padlock = Padlock::new();
         padlock.current = 1;
         let rot = Rotation::new(Direction::L, 2);
-        let location = padlock.get_next_location(rot);
+        let location = padlock.get_next_location(&rot);
         assert_eq!(location, 99);
     }
     #[test]
     fn test_rotate_left_1() {
         let padlock = Padlock::new();
         let rot = Rotation::new(Direction::L, 2);
-        let location = padlock.get_next_location(rot);
+        let location = padlock.get_next_location(&rot);
 
         assert_eq!(location, 48);
     }
@@ -235,7 +346,7 @@ mod tests {
     fn test_rotate_left_3() {
         let padlock = Padlock::new();
         let rot = Rotation::new(Direction::L, 68);
-        let location = padlock.get_next_location(rot);
+        let location = padlock.get_next_location(&rot);
         assert_eq!(location, 82);
     }
     #[test]
@@ -248,6 +359,41 @@ mod tests {
     fn test_read_test_input() -> Result<()> {
         let rotations = parse_day_1_input("input/day_1_test_input.txt")?;
         assert!(rotations.len() == 10);
+        Ok(())
+    }
+    #[test]
+    fn test_eval_test_input() -> Result<()> {
+        let mut padlock = Padlock::new();
+        let rotations = parse_day_1_input("input/day_1_test_input.txt")?;
+        let total_zeros = padlock.evaluate(&rotations);
+        println!("Total Zeroes In Test Input: {total_zeros}");
+
+        Ok(())
+    }
+    #[test]
+    fn test_eval_input() -> Result<()> {
+        let mut padlock = Padlock::new();
+        let rotations = parse_day_1_input("input/day_1_input.txt")?;
+        let total_zeros = padlock.evaluate(&rotations);
+        println!("Total Zeroes In Input: {total_zeros}");
+        Ok(())
+    }
+    #[test]
+    fn test_eval_test_input_tracked() -> Result<()> {
+        let mut padlock = Padlock::new();
+        let rotations = parse_day_1_input_tracked("input/day_1_test_input.txt")?;
+        let total_zeros = padlock.evaluate_tracked(&rotations);
+        println!("Total Zeroes In Tracked Test Input: {total_zeros}");
+
+        Ok(())
+    }
+    #[test]
+    fn test_eval_input_tracked() -> Result<()> {
+        let mut padlock = Padlock::new();
+        let rotations = parse_day_1_input_tracked("input/day_1_input.txt")?;
+        let total_zeros = padlock.evaluate_tracked(&rotations);
+        println!("Total Zeroes In Track Input: {total_zeros}");
+
         Ok(())
     }
 }
